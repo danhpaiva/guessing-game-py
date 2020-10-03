5422b7ad09f686aa14da2b885116b10c4be850ca
Figur8 <ismael_profissional@hotmail.com>
2020-10-03 14:52:10 -0300

    Applying SRP on code and resolve a numeric bug when get input number after the first role

--- game.py
+++ game.py
@@ -1,33 +1,35 @@
 import os
 import random
 
-tentativas = 0
 numeroSorteado = random.randrange(0, 100)
-letras = "abcdefghijklmnopqrstuvxz"
 
-os.system("CLS")
-print("Jogo de Adivinhação\n")
-
-nome = input("Primeiro, nos diga seu nome: ")
-
-print(nome + " estamos sorteando o número... Número sorteado!!!")
-
-numeroDigitado = input(
-    "Digite um número para tentar adivinhar qual número foi sorteado por nossa máquina: ")
-while numeroDigitado in letras:
-    numeroDigitado = input(
-        "Digite um número para tentar adivinhar qual número foi sorteado por nossa máquina: ")
-
-numeroDigitado = int(numeroDigitado)
-
-while numeroDigitado != numeroSorteado:
+def introducePlayer():
     os.system("cls")
+    print("Jogo de Adivinhação\n")
+    nome = input("Primeiro, nos diga seu nome: ")
+    print(nome + " estamos sorteando o número... Número sorteado!!!")
+
+def getTipAboutSelectedNumber(numeroSorteado, numeroDigitado):
     if numeroSorteado > numeroDigitado:
         print("\tO número sorteado é maior...")
     elif numeroSorteado < numeroDigitado:
         print("\tO número sorteado é menor...")
-    tentativas += 1
-    numeroDigitado = int(input("Digite outro número: \n"))
 
-print("\nNúmero: " + str(numeroDigitado) + ", você precisou de " +
-      str(tentativas + 1) + " tentativas.")
+def searchNumber(numeroDigitado):
+    tentativas = 0
+
+    while numeroDigitado != numeroSorteado:
+        os.system("cls")
+        if not numeroDigitado.isnumeric():
+            numeroDigitado = input("Digite um número para tentar adivinhar qual número foi sorteado por nossa máquina: ")
+
+        getTipAboutSelectedNumber(numeroSorteado, int(numeroDigitado))
+        numeroDigitado = input("Digite outro número: \n")
+        tentativas += 1
+
+    print("\nNúmero: " + str(numeroDigitado) + ", você precisou de " + str(tentativas + 1) + " tentativas.")
+
+introducePlayer()
+searchNumber(input("Digite um número para tentar adivinhar qual número foi sorteado por nossa máquina: "))
+
+
